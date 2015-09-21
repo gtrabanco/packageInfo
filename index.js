7c9523bff23c30cf3271a1972a2ce360f0fbcffd
Gabriel Trabanco Llano <gtrabanco@fwok.org>
2015-09-21 20:25:43 +0430

    Added improvements to the library to debug and functionality

--- index.js
+++ index.js
@@ -12,10 +12,12 @@
 
         //We need fs module
         var fs = require("fs");
+        var path = require("path");
 
         //Check if we have access to the file
         var stats = fs.statSync(packageFile);
 
+        //We need a file, if not just do nothing
         if (stats.isFile()) {
             try {
                 fs.accessSync(packageFile, fs.F_OK | fs.R_OK);
@@ -26,24 +28,29 @@
 
                 //Just to make us sure we didn't process a different file and retrieve a secure trust information
                 //about the package
-                if(!packageInfo.name || !packageInfo.author || !packageInfo.version) {
+                if(!packageInfo.name || !packageInfo.version) {
                     packageInfo = false;
                 }
+
             } catch (e) {
                 packageInfo = false;
             }
+        } else if(stats.isDir()) {
+            //If it is dir, try if it is our lucky day and search if there is any package.json
+            return readPackageInfo(path.join(packageFile, "package.json"));
         }
 
         return packageInfo;
     }
 
-    //The exercise function
+//The exercise function
     function moduleVersion(module, callBack) {
         var path = require("path");
         var fs = require("fs");
         var node_modules = path.join(".", "node_modules");
         var error = false;
 
+        //Check if it is accessible the node_module dir
         try {
             fs.accessSync(node_modules, fs.R_OK);
 
@@ -51,39 +58,86 @@
 
             var packageInfo = readPackageInfo(modulePath);
 
-            var error = packageInfo === false;
+            error = packageInfo === false;
         } catch (e) {
-            console.log(e);
             error = true;
         }
 
-        callBack(error, packageInfo);
+        //If no error callBack returning the results
+        if (callBack) {
+            return callBack(error, packageInfo);
+        } else{
+            return packageInfo;
+        }
+    }
+
+//This read a package.json and its dependencies in node_modules giving the function a path
+//to the app and two callbacks for the app packaje.json and other for each dependency
+//package.json
+    function readDependencies(pkgPath, packageCallBack, dependencyCallBack) { //cb(error, dependencyModuleInfo)
+        var path = require("path");
+        var currentPackage = readPackageInfo(path.join(pkgPath, "package.json"));
+
+        //If no error continue or die!
+        if (currentPackage !== false) {
+            var modulesInfo = []; //Initializes an array due to we don't want an error when we will do .push
+
+            var returnedPackage = currentPackage;
+
+            //If there is any callback modify future returned value
+            if (packageCallBack) {
+                returnedPackage = packageCallBack(false, currentPackage);
+            }
+
+            var modules = Object.getOwnPropertyNames(returnedPackage.dependencies);
+
+
+            //If the module has any dependency we process it
+            if (modules.length > 0) {
+                modules.forEach(function (module) {
+                    //If there is any callback for the dependency
+                    if (dependencyCallBack){
+                        modulesInfo.push(moduleVersion(module, dependencyCallBack));
+                    } else if (packageCallBack) {
+                        //Maybe the developer wants only one callback for dependencies and the original package
+                        modulesInfo.push(moduleVersion(module, packageCallBack));
+                    } else {
+                        //Maybe the developer only wants a returned value
+                        modulesInfo.push(moduleVersion(module));
+                    }
+
+                });
+            }
+
+            return [returnedPackage, modulesInfo];
+        } else {
+            if (packageCallBack && dependencyCallBack) {
+                return [packageCallBack("The package.json could not be found. Check if exists, is accessible and the permissions."),
+                    dependencyCallBack("The original package could not be loaded. Check if the file exists.")];
+            } else if (packageCallBack) {
+                return [packageCallBack("The package.json could not be found. Check if exists, is accessible and the permissions."),
+                    packageCallBack("The original package could not be loaded. Check if the file exists.")];
+            } else {
+                return [false, false];
+            }
+        }
     }
 
     //The real program begin here
     //Reading this package.json
-    var currentPackage = readPackageInfo("package.json");
+    readDependencies("./", function(error, packageInfo){
+            console.log("For the current package %s (v. %s)", packageInfo.name, packageInfo.version);
+            console.log("The dependencies are:");
+        },
+        function(error, packageInfo){
+            if (error) {
+                console.log(error);
+                return;
+            }
 
-
-    if (currentPackage !== false) {
-        console.log("For the current package %s (v. %s)", currentPackage.name, currentPackage.version);
-        console.log("The dependencies are:");
-
-        //Reading the dependencies
-        for (var module in currentPackage.dependencies) {
-            var moduleInfo = moduleVersion(module, function(error, packageInfo) {
-                if (error) {
-                    console.log("Could not read %s module package.json, perhaps npm install?", module);
-                    return;
-                }
-
-                console.log("\tThe dependency module \"%s\" version is %s", packageInfo.name, packageInfo.version);
-                console.log("\tThe original author is: %s <%s>", packageInfo.author.name || "No name", packageInfo.author.email || "No email address");
-                console.log("\tDon't forget it!\n");
-            });
-        }
-    } else {
-        console.log("Could not read the package.json");
-    }
+            console.log("\tThe dependency module \"%s\" version is %s", packageInfo.name, packageInfo.version);
+            console.log("\tThe original author is: %s <%s>", packageInfo.author.name || "No name", packageInfo.author.email || "No email address");
+            console.log("\tDon't forget it!\n");
+        });
 
 }());
